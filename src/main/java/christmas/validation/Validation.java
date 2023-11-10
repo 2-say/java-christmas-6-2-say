59e35c3d0cd556b8a034dd2360ec28a72da71f0c
sehee <tpgml701@naver.com>
2023-11-10 16:54:18 +0900

    feat(Validation): 날짜 범위 검증 기능 구현

--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -3,7 +3,9 @@ package christmas.validation;
 public final class Validation {
 
     public static void validateDayInRange(int day) {
-
+        if (day < 1 || day > 31){
+            throw new IllegalArgumentException();
+        }
     }
 
 
