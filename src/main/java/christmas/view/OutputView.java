67ed1bc5d810cbc4a8f53fc5aeb8a5a7eba5178c
sehee <tpgml701@naver.com>
2023-11-10 17:40:31 +0900

    feat(OutputView): infoEvent 메시지("12d월 26일에..") 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,10 +2,14 @@ package christmas.view;
 
 public class OutputView {
 
-    final static String INFO_MESSAGE= "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    final static String INFO_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    final static String INFO_EVENT_MESSAGE = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
-
-    public static void InfoMessage() {
+    public static void infoMessage() {
         System.out.println(INFO_MESSAGE);
+    }
+
+    public static void infoEventMessage() {
+        System.out.println(INFO_EVENT_MESSAGE);
     }
 }
