c05c4fdd6378f1b247c906015ab02dc788107a29
sehee <tpgml701@naver.com>
2023-11-10 16:43:52 +0900

    test(validation): 날짜가 범위에 포함 되는지 검증하는 기능

---
+++ src/test/java/christmas/validation/ValidationTest.java
@@ -0,0 +1,38 @@
+package christmas.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.validation.Validator;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+public class ValidationTest {
+
+    @Test
+    void validateDayInRange_테스트() {
+        // given
+        List<Integer> case1 = List.of(1,2,3);
+        List<Integer> case2 = List.of(0);
+        List<Integer> case3 = List.of(31,32);
+
+        // when
+        Throwable result1 =  catchThrowable(() ->{
+            Validator.validateDayInRange(case1.get(0),case1.get(1),case1.get(2));
+        });
+        Throwable result2 =  catchThrowable(() ->{
+            Validator.validateDayInRange(case2.get(0));
+        });
+        Throwable result3 =  catchThrowable(() ->{
+            Validator.validateDayInRange(case3.get(0),case3.get(1));
+        });
+
+        // then
+        assertThat(result1).doesNotThrowAnyException();
+        assertThat(result2).isInstanceOf(IllegalArgumentException.class);
+        assertThat(result3).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
