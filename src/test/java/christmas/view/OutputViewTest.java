9154ac58a0f4854ad85114dbbf6cca81ab0ae22a
sehee <tpgml701@naver.com>
2023-11-10 17:40:12 +0900

    test(OutputViewTest): infoEvent 메시지("12d월 26일에..") 출력 검증 기능

--- src/test/java/christmas/view/OutputViewTest.java
+++ src/test/java/christmas/view/OutputViewTest.java
@@ -1,26 +1,41 @@
 package christmas.view;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OutputViewTest {
 
+    @BeforeEach
+    void testSetup() {
+    }
+
     @Test
-    void infoMessage_테스트(){
+    void infoMessage_테스트() {
         // given
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         // when
-        OutputView.InfoMessage();
+        OutputView.infoMessage();
         String capturedOutput = outContent.toString().trim();
         // then
         assertEquals("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.", capturedOutput);
     }
+
+    @Test
+    void infoEventMessage_테스트() {
+        // given
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        // when
+        OutputView.infoEventMessage();
+        String capturedOutput = outContent.toString().trim();
+        // then
+        assertEquals("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", capturedOutput);
+    }
+
 }
