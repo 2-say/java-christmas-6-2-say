42e70db0644221a21c28e2d3c8894134108ccd2f
sehee <tpgml701@naver.com>
2023-11-10 17:34:04 +0900

    feat(OutputView): info 메시지("안녕하세요..") 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,11 @@
 package christmas.view;
 
 public class OutputView {
+
+    final static String INFO_MESSAGE= "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+
+    public static void InfoMessage() {
+        System.out.println(INFO_MESSAGE);
+    }
 }
