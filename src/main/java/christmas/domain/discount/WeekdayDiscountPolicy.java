6b4f08eeb8c5d9518722a9d49c4614a384d4b3e3
sehee <tpgml701@naver.com>
2023-11-11 21:06:01 +0900

    feat(DiscountPolicy): 주말, 평일 할인 기능

--- src/main/java/christmas/domain/discount/WeekdayDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscountPolicy.java
@@ -3,13 +3,12 @@ package christmas.domain.discount;
 import christmas.domain.Bilge;
 import christmas.domain.Menu;
 
-public class WeekendDiscountPolicy implements DiscountPolicy{
-
+public class WeekdayDiscountPolicy implements DiscountPolicy{
     private int discountAmount = 2023;
     @Override
     public int discount(Bilge bilge, int price) {
         return bilge.getBilge().stream()
-                .filter(menu -> menu.getCategory() == Menu.Category.MAIN)
+                .filter(menu -> menu.getCategory() == Menu.Category.DESSERT)
                 .mapToInt(menu -> price - discountAmount)
                 .sum();
     }
