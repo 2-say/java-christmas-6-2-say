6fcdb36803a63e4fa19e12343bd0721bd86d90f3
sehee <tpgml701@naver.com>
2023-11-11 15:05:46 +0900

    test(ValidationTest): parse int 검증

--- src/test/java/christmas/validation/ValidationTest.java
+++ src/test/java/christmas/validation/ValidationTest.java
@@ -1,10 +1,8 @@
 package christmas.validation;
 
-import org.assertj.core.api.Assertions;
+import christmas.utils.Utils;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.validation.Validator;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
@@ -19,13 +17,13 @@ public class ValidationTest {
         int case3 = 0;
 
         // when
-        Throwable result1 =  catchThrowable(() ->{
+        Throwable result1 = catchThrowable(() -> {
             Validation.validateDayInRange(case1);
         });
-        Throwable result2 =  catchThrowable(() ->{
+        Throwable result2 = catchThrowable(() -> {
             Validation.validateDayInRange(case2);
         });
-        Throwable result3 =  catchThrowable(() ->{
+        Throwable result3 = catchThrowable(() -> {
             Validation.validateDayInRange(case3);
         });
 
@@ -34,5 +32,20 @@ public class ValidationTest {
         assertThat(result2).isInstanceOf(IllegalArgumentException.class);
         assertThat(result3).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void validateStringToInteger_Test() {
+        // given
+        String input = "1a";
+        // when
+        Throwable result = catchThrowable(() -> {
+            Validation.validateStringToInteger(input);
+        });
+        // then
+        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
 
 }
