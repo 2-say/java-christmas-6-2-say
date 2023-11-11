2f50b9c0a90acd93a19ce889a8879fbe5c234f41
sehee <tpgml701@naver.com>
2023-11-11 21:05:41 +0900

    test(DiscountPolicy): 주말, 평일 할인 정책 검증

--- src/test/java/christmas/domain/discount/DiscountPolicyTest.java
+++ src/test/java/christmas/domain/discount/DiscountPolicyTest.java
@@ -6,18 +6,20 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public class DiscountPolicyTest {
 
-
-    @DisplayName("주말 할인 테스트")
+    @DisplayName("평일 할인 테스트")
     @Test
-    void weekendDiscountPolicyTest() {
+    void weekdayDiscountPolicyTest() {
         // given
         List<Menu> bilgeCase1 = List.of(Menu.CHOCOCAKE);
-        Bilge bilge = new Bilge(bilgeCase1);
-        DiscountPolicy discountPolicy = new WeekendDiscountPolicy();
+        LocalDate today = LocalDate.of(2023,12,1);
+        Bilge bilge = new Bilge(today,bilgeCase1);
+        DiscountPolicy discountPolicy = new WeekdayDiscountPolicy();
 
         // when
         int discount = discountPolicy.discount(bilge, Menu.CHOCOCAKE.getPrice());
@@ -25,4 +27,23 @@ public class DiscountPolicyTest {
         // then
         Assertions.assertThat(discount).isEqualTo(12977);
     }
+
+
+    @DisplayName("주말 할인 테스트")
+    @Test
+    void weekendDiscountPolicyTest() {
+        // given
+        List<Menu> bilgeCase1 = List.of(Menu.BARBECUERIB);
+        LocalDate today = LocalDate.of(2023,12,1);
+        Bilge bilge = new Bilge(today,bilgeCase1);
+        DiscountPolicy discountPolicy = new WeekendDiscountPolicy();
+
+        // when
+        int discount = discountPolicy.discount(bilge, Menu.BARBECUERIB.getPrice());
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(51977);
+    }
+
+
 }
