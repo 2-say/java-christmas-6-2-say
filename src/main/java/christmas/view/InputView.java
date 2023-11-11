bfc8d004d44afc0b8ea82eb7b6a0afdd5e09cc34
sehee <tpgml701@naver.com>
2023-11-11 14:49:09 +0900

    feat(InputView): 사용자 입력 기능, parse int return 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,16 +1,21 @@
 package christmas.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import christmas.utils.Utils;
 
+public class InputView {
     public static String INPUT_DAY_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요?";
     public static String INPUT_MENU_MESSAGE ="주문하실 메뉴와 개수를 알려 주세요.";
 
     public static void inputDayMessage() {
         System.out.println(INPUT_DAY_MESSAGE);
+        String input = Console.readLine();
     }
 
-    public static void inputMenuMessage() {
+    public static int inputMenuMessage() {
         System.out.println(INPUT_MENU_MESSAGE);
+        String input = Console.readLine();
+        return Utils.stringToInteger(input);
     }
 
 
