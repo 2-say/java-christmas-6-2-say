d9ce67ee7ea5a53393c8fbad65f47d39cf9e1cc7
sehee <tpgml701@naver.com>
2023-11-11 22:31:40 +0900

    feat(DiscountService): 여러 할인을 관리하는 Service

---
+++ src/main/java/christmas/service/DiscountService.java
@@ -0,0 +1,34 @@
+package christmas.service;
+
+import christmas.domain.Bilge;
+import christmas.domain.discount.DiscountPolicy;
+import christmas.domain.discount.WeekdayDiscountPolicy;
+import christmas.domain.discount.WeekendDiscountPolicy;
+import christmas.utils.DateUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountService {
+
+    private Bilge bilge;
+    private List<DiscountPolicy> discountPolicies;
+
+    public DiscountService(Bilge bilge) {
+        this.bilge = bilge;
+    }
+
+    public void events() {
+        if (DateUtils.isDecember(bilge.getDate()) && DateUtils.isWeekday(bilge.getDate()) && DateUtils.isDayOfMonthInRange(bilge.getDate(), 1, 31)) {
+            discountPolicies.add(new WeekdayDiscountPolicy());
+        }
+
+        if (DateUtils.isDecember(bilge.getDate()) && DateUtils.isWeekend(bilge.getDate()) && DateUtils.isDayOfMonthInRange(bilge.getDate(), 1, 31)) {
+            discountPolicies.add(new WeekendDiscountPolicy());
+        }
+
+        if (DateUtils.isStarDay(bilge.getDate())) {
+//            discountPolicies.add(new SpecialDiscountPolicy());
+        }
+    }
+}
