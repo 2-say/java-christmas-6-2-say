9c214d73de608a2601fa44f671e1de77b265cfe1
sehee <tpgml701@naver.com>
2023-11-11 14:25:10 +0900

    feat(View): 출력 메시지 기능

---
+++ src/main/java/christmas/view/ErrorView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+public class ErrorView {
+
+    final static String INPUT_DAY_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+
+    public static void inputDayError() {
+        System.out.println(INPUT_DAY_ERROR_MESSAGE);
+    }
+
+
+
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,17 @@
 package christmas.view;
 
 public class InputView {
+
+    public static String INPUT_DAY_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요?";
+    public static String INPUT_MENU_MESSAGE ="주문하실 메뉴와 개수를 알려 주세요.";
+
+    public static void inputDayMessage() {
+        System.out.println(INPUT_DAY_MESSAGE);
+    }
+
+    public static void inputMenuMessage() {
+        System.out.println(INPUT_MENU_MESSAGE);
+    }
+
+
 }
