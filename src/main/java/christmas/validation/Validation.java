bb369d34e6f27b04dd65a0f3f9b608005a9738df
sehee <tpgml701@naver.com>
2023-11-12 22:20:13 +0900

    feat(Validation): 메뉴 중복 이름 입력 검증 , Dash분리 검증, Comma분리 검증, 숫자형식 검증 기능

--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -1,5 +1,11 @@
 package christmas.validation;
 
+import christmas.domain.Menu;
+import christmas.view.ErrorView;
+
+import java.util.Arrays;
+import java.util.Set;
+
 public final class Validation {
 
     public static void validateDayInRange(int day) {
@@ -10,9 +16,54 @@ public final class Validation {
 
     public static void validateStringToInteger(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
+            ErrorView.inputDayError();
             throw new IllegalArgumentException();
         }
     }
+
+    public static void validateMenuName(String name) {
+        if (Arrays.stream(Menu.values())
+                .noneMatch(menu -> menu.getName().equalsIgnoreCase(name))) {
+            ErrorView.inputMenuError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkDuplicateMenuName(Set<String> seenMenuNames, String menuName) {
+        if (!seenMenuNames.add(menuName)) {
+            ErrorView.inputMenuError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            ErrorView.inputMenuError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    public static void validateMenuFormat(String[] str) {
+        if(str.length!=2){
+            ErrorView.inputMenuError();
+            throw new IllegalArgumentException();
+        }
+    }
+    public static void validateDashSplitter(String input) {
+        if (!input.contains("-")) {
+            ErrorView.inputMenuError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
+
+
+
 
 
 
