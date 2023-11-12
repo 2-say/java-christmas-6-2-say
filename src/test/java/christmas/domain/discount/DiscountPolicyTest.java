82197c83a833ca78c49e6935f66cdc0e3b44f449
sehee <tpgml701@naver.com>
2023-11-12 22:21:36 +0900

    test(DiscountPolicyTest): 특별할인 검증 , 샴페인 증정 검증 기능

--- src/test/java/christmas/domain/discount/DiscountPolicyTest.java
+++ src/test/java/christmas/domain/discount/DiscountPolicyTest.java
@@ -46,7 +46,53 @@ public class DiscountPolicyTest {
     }
 
 
+    @DisplayName("특별 할인 테스트")
+    @Test
+    void specialDiscountPolicyTest() {
+        // given
+        Map<Menu,Integer> bilgeCase1 = Map.of(Menu.TIBONESTEAK,2);
+        LocalDate today = LocalDate.of(2023,12,3);
+        Bilge bilge = new Bilge(today,bilgeCase1);
+        DiscountPolicy discountPolicy = new SpecialDiscountPolicy();
+
+        // when
+        int discount = discountPolicy.discount(bilge);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @DisplayName("크리스마스 누적 할인 테스트")
+    @Test
+    void christmasDiscountPolicyTest() {
+        // given
+        Map<Menu,Integer> bilgeCase1 = Map.of(Menu.TIBONESTEAK,2);
+        LocalDate today = LocalDate.of(2023,12,12);
+        Bilge bilge = new Bilge(today,bilgeCase1);
+        DiscountPolicy discountPolicy = new ChristmasDiscountPolicy();
+
+        // when
+        int discount = discountPolicy.discount(bilge);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(2100);
+    }
 
 
+    @DisplayName("샴페인 할인 테스트")
+    @Test
+    void champagneDiscountPolicyTest() {
+        // given
+        Map<Menu,Integer> bilgeCase1 = Map.of(Menu.TIBONESTEAK,2);
+        LocalDate today = LocalDate.of(2023,12,12);
+        Bilge bilge = new Bilge(today,bilgeCase1);
+        DiscountPolicy discountPolicy = new ChampagneDiscountPolicy();
+
+        // when
+        int discount = discountPolicy.discount(bilge);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(25000);
+    }
 
 }
