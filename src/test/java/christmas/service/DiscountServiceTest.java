d291d1086dd83b183bc1a094148709b4b90c007a
sehee <tpgml701@naver.com>
2023-11-12 22:33:37 +0900

    test(DiscountService): 여러 할인 검증 테스트

--- src/test/java/christmas/service/DiscountServiceTest.java
+++ src/test/java/christmas/service/DiscountServiceTest.java
@@ -9,21 +9,35 @@ import java.time.LocalDate;
 import java.util.Map;
 
 public class DiscountServiceTest {
-
-
     @Test
-    void 주말_할인_테스트 () {
+    void 평일_스타데이_크리스마스누적_할인_테스트() {
         // given
-
-        Map<Menu,Integer> bilgeCase1 = Map.of(Menu.TIBONESTEAK,2);
-        LocalDate today = LocalDate.of(2023,12,3);
-        Bilge bilge = new Bilge(today,bilgeCase1);
+        Map<Menu, Integer> bilgeCase1 = Map.of(Menu.CHOCOCAKE, 2);
+        LocalDate today = LocalDate.of(2023, 12, 3);
+        Bilge bilge = new Bilge(today, bilgeCase1);
         DiscountService discountService = new DiscountService(bilge);
 
         // when
         int totalDiscount = discountService.applyDiscount();
 
         // then
-        Assertions.assertThat(totalDiscount).isEqualTo(2200);
+        Assertions.assertThat(totalDiscount).isEqualTo(4046+1000+1200);
     }
+
+    @Test
+    void 주말_크리스마스누적_할인_테스트() {
+        // given
+        Map<Menu, Integer> bilgeCase1 = Map.of(Menu.BARBECUERIB, 2);
+        LocalDate today = LocalDate.of(2023, 12, 15);
+        Bilge bilge = new Bilge(today, bilgeCase1);
+        DiscountService discountService = new DiscountService(bilge);
+
+        // when
+        int totalDiscount = discountService.applyDiscount();
+
+        // then
+        Assertions.assertThat(totalDiscount).isEqualTo(4046+2400);
+    }
+
+
 }
