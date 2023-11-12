fe84b5613ea260ae63b4fe272f528af993ca5213
sehee <tpgml701@naver.com>
2023-11-12 22:43:35 +0900

    feat(ErrorView): "유효하지 않은 주문" 출력 기능

--- src/main/java/christmas/view/ErrorView.java
+++ src/main/java/christmas/view/ErrorView.java
@@ -3,12 +3,15 @@ package christmas.view;
 public class ErrorView {
 
     final static String INPUT_DAY_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-
+    final static String INPUT_MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public static void inputDayError() {
         System.out.println(INPUT_DAY_ERROR_MESSAGE);
     }
 
+    public static void inputMenuError() {
+        System.out.println(INPUT_MENU_ERROR_MESSAGE);
+    }
 
 
 }
